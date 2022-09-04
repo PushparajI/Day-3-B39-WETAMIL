637d7e56820735eb8ad0c6aed3e20912457a410d
PushparajI <110769441+PushparajI@users.noreply.github.com>
2022-09-04 15:19:07 +0530

    Add files via upload

---
+++ forlooping.js
@@ -0,0 +1,32 @@
+// Iteration / looping 
+
+// Print the followings
+// 1 x 1 = 1
+// 2 x 2 = 4
+// 3 x 3 = 6
+// .
+// .
+// .
+// 10 x 10 = 100
+
+// for (var i = 1; i <=10; i++) {
+//   console.log(i + "x" + i + "=" + i * i);
+// }
+
+
+
+// Iteration for of
+// No Increment variable needed
+var arr = ["1 One", "2 Two", "3 Three", "4 Four", "5 Five", "6 Six"];
+
+// for (var z of arr) {
+//   console.log(z);
+// } 
+
+
+// Iteration for in
+// iterates the index of the array
+
+// for (var i in arr) {
+//     console.log(i);
+// }
